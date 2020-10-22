ead654645c084d25f2ea2ee44a6df2777dafb0e1
Dan <up765064@myport.ac.uk>
2020-10-22 14:19:35 +0100

    grid_done

--- main JS/grid.js
+++ main JS/grid.js
@@ -12,12 +12,24 @@ BEHAVIOUR
 //Active player.
 //Main array. 6 arrays inside which are rows. Each row array has 7 items in it.
 
+
+
 class Grid {
     constructor(){
         this.activePlayer = true;
         this.grid = []
-
-        
+        let id = 1;
+        for(let i = 1; i <= 6; i++){
+            this.grid.push(createRow())
+        }
+        function createRow (){
+            let row = [];
+            for(let i =1; i <= 7; i++){
+                row.push(new Slot(id))
+                id++;
+            }
+            return row;
+        }
     }
     playerOne(){
         this.activePlayer = true;
--- main JS/players.js
+++ main JS/players.js
@@ -1,13 +1,12 @@
 /*PLAYER:
-
 STATE
 -id 
 -name (name for player 1 and player 2/computer)
 -colour (red/yellow)
 -slot position (start at neutral/blank)
-
 BEHAVIOUR
 -Choose name?
 -set colour?
 -Choose slot method (1-7)
 */
+
